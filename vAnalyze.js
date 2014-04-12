3cee65533154ccce55350fbeac5c6291bf7fbf51
Daniel Shumway <shumway.danny@gmail.com>
2014-04-12 13:38:05 -0400

    Functions are wrapped now.  There's a tiny tiny bit of slowdown, but not too bad. Getting a weird error with Cookie Clicker, the current test app.  I think they're attaching something to their functions which isn't getting carried over.  I'll try to fix it, but it isn't a huge emergency - I think it's them doing something weird, and most programs we won't run into it with.
    Signed-off-by: Daniel Shumway <shumway.danny@gmail.com>

--- vAnalyze.js
+++ vAnalyze.js
@@ -9,7 +9,9 @@ var vAnalyze_base_constructor = function() {
         this.date = new Date();
         this.currentlyInfectedMethods = 0;
         this.currentlyIteratedObjects = 0;
+        this.callCount = 0;
 
+        //A method that infects all methods on 
         this.infection = function(elementToInfect, infectionFrame){
             for (var property in elementToInfect)
             {
@@ -17,14 +19,31 @@ var vAnalyze_base_constructor = function() {
                  try {
                     if (typeof elementToInfect[property] == "function") {
                         var theFunction = elementToInfect[property].toString().split(";");
-                        //Don't worry about functions that are blank or nativxe code.  This is a hacky way of doing this, but it appears to work.
+                        //Don't worry about functions that are blank or native code.  This is a hacky way of doing this, but it appears to work.
                         if(theFunction.length >= 2) {
-                            //Don&#39;t infect your own methods.
+                            //Don't infect your own methods.
                             if(elementToInfect[property].vAnalyze_infected == undefined)
                             { 
-                                elementToInfect[property].vAnalyze_infected = 1;
+                                elementToInfect[property].vAnalyze_infected = 1; //Mark as infected.
+                                var functionInfecting = elementToInfect[property]; //Save a reference to the old function.
+
+                                //We're using the reference elementToInfect[property] to get around reference errors.  If we just said ourFunctionReference = new function()
+                                //it wouldn't replace the actual function, just our reference to it.
+                                //It's like dealing with pointers.
+                                elementToInfect[property] = function() { 
+                                    //Here we should check in with our base of operations and run whatever code is requested.
+                                    vAnalyze_base.callCount++;
+
+                                    //Call the old method, but also pass in the arguments it should have.
+                                    //And yes, we pass in *this*, because this will mean the correct context when the program is actually run.
+                                    var args = Array.prototype.slice.call(arguments); //Convert parameters to an array.
+                                    arguments.callee.vAnalyze_oldCode.apply(this, args); //Call the original method.
+                                }
+
+                                //By the way, we do need to attach the old code.
+                                elementToInfect[property].vAnalyze_oldCode = functionInfecting;
+
                                 this.currentlyInfectedMethods++;
-                                //console.log(elementToInfect + ": " + property); 
                             }
                         }
                     } else if (elementToInfect[property] != null && typeof elementToInfect[property] == "object") {
@@ -76,11 +95,7 @@ var vAnalyze_base_constructor = function() {
                 this.lastReset = this.date.getTime();
                 //console.log("newTime is " + this.lastReset + " and getTime is " + this.date.getTime());
             }
-            else
-            {
-                //console.log(this.lastReset);
-            }
-
+            //..a thing.. that should be commented.
             if(this.toInfect.length > 0) {
                 var elementToInfect = this.toInfect.shift();
                 this.infection(elementToInfect, this.infectionsRun);
@@ -88,7 +103,7 @@ var vAnalyze_base_constructor = function() {
             }
             else
             {
-                console.log("Restarting, iterated through " + this.currentlyIteratedObjects + " objects.  Currently infected methods: " + this.currentlyInfectedMethods);
+                //console.log("Restarting, iterated through " + this.currentlyIteratedObjects + " objects.  Currently infected methods: " + this.currentlyInfectedMethods);
                 this.currentlyIteratedObjects = 0;
                 this.startInfection(this.base);
             }
