ae3f5754b9e0c2e48f0a425914345744abb22b05
Daniel Shumway <shumway.danny@gmail.com>
2015-04-07 22:44:35 -0400

    feat (base functionality): wrapping private variables and adding them as properties
    Starts refactor of infections into subgroups.
    Fixes function wrapping to be more flexible.
    Private variables can now be wrapped in monitor-able getters and setters using eval method.
    Breaks previous example (probably).

--- index.js
+++ index.js
@@ -96,77 +96,138 @@ function getOrigin(that, property) {
     return getOrigin(that.__proto__, property); //ToDo: double check that this isn't depreciated.
 }
 
+
 /**
-* 
-* ToDo: Fill out documentation.
-* 
-**/
-function wrap(that, owner, name) {
-	var original, replacement;
+*
+* @param property - name of the property
+* @param template (optional) - object that specifies custom getter or setter.
+*/
+function property(host, property, template) {
 
-	if(!owner || (!name && name !== 0)) { return that; }
-	//Don't use hasOwnProperty here since functions can be attached to global context.
-	if(owner.hasOwnProperty(name)) {
-		original = owner[name];
-
-		//Build function wrapper
-		replacement = original.__wrapper__ || function() {
-			var args = Array.prototype.slice.call(arguments),
-				toReturn;
-
-			//Start
-			helpers.before(replacement.__infection__);
-
-			//Fire
-			stack.push(original);
-			toReturn = original.apply(this, args);
-
-			//Infect the returned value (in case we're in a factory).
-			//ToDo:  We don't know the owner?  Isn't the owner whatever method called this one?  
-			//Heck, isn't the owner this method?  Might not be the *owner*, but it's certainly the creator.  Are those different?
-			//stack.peek or something should be passed in instead of undefined.  We certainly don't know the name though.
-			if(toReturn && toReturn.infect) { 
-				toReturn.infect(undefined, undefined); 
-			};
-			//Infect this (in case we're in a Constructor).
-			if(this.infect) {
-				this.infect(undefined, undefined);
-			}
-			
-			//Finish
-			helpers.after(replacement.__infection__);
-
-			stack.pop();
-
-			return toReturn;
-		};
-
-		//Set prototype correctly.
-		//ToDo: Set Constructor.
-		replacement.prototype = original.prototype;
-
-		//If this function was already distributed through code, we might accidentally infect/wrap it again.
-		//In that case, what we really want to do is update the function to be using the wrapper we already made.
-		Object.defineProperty(original, '__wrapper__', {value: replacement, enumerable:false });
-
-		//Attach properties.
-		for(var p in original) {
-			if(original.hasOwnProperty(p)) {
-				replacement[p] = original[p];
-			}
-		}
-
-		//Fix toString.
-		replacement.toString = function() {
-			return original.toString();
-		}
-
-		//And attach.
-		owner[name] = replacement;
-		return replacement;
+	//Make sure host is suitable.
+	if(!host.__infection__) {
+		host.infect();
 	}
 
-	return that;
+	var infection = host.__infection__,
+		_property = {
+			name: property,
+			host: host,
+			//
+			get: function () { return null; },
+			set: function () { },
+			watch: function() { },
+			//
+			history: [],
+			onChange: function() { //ToDo: fill out.
+
+			}
+		};
+
+	if(template) {
+
+		_property.get = template.get || _property.get;
+		_property.set = template.set || _property.get;
+		_property.alias = template.id || _property.id;
+	}
+
+	host.__infection__.properties[property] = _property;
+}
+
+/**
+* Allows you to wrap private variables as if they were functions.
+* 
+* Usage: 
+* var x = 5;
+* var y = 10;
+* eval(obj.privateWrapper(['x', 'y']));
+* 
+* @param variables - an array of private variables to wrap as properties.
+* @returns - a string that should be evaled in the current scope.
+*/
+function localPropertyCode(variables) {
+
+	var _code = "",
+		i,j;
+
+	for(i=0,j=variables.length; i<j; i++){
+		_code = _code + 
+		"Object.infect.property( this, '" + variables[i] + "', {" +
+				"get : function() { " +
+					"return " + variables[i] + ";" +
+				"}, " +
+				"set : function(value) { " +
+					variables[i] + " = value;" + 
+				"}" +
+		" }); ";	
+	}
+
+	return _code;
+}
+
+function wrapFunction(that) {
+	var original, replacement;
+
+	//Non-functions can't be wrapped.
+	if(typeof that !== 'function') { return that; }
+
+	original = that;
+
+	//Build function wrapper if it doesn't already exist.
+	replacement = original.__wrapper__ || function() {
+		var args = Array.prototype.slice.call(arguments),
+			toReturn;
+
+		//Start
+		helpers.before(replacement.__infection__);
+
+		//Fire
+		stack.push(replacement); //push the replacement - toString already works with it, so that's all we care about.
+		toReturn = original.apply(this, args);
+
+		//Infect the returned value (in case we're in a factory).
+		if(toReturn && toReturn.infect) { 
+			toReturn = wrapFunction(toReturn); //Will return original value if not a function.
+			toReturn.infect(undefined, undefined); //ToDo: think about whether or not anything ought to be passed in here.
+		};
+		//Infect this (in case we're in a Constructor).
+		if(this.infect) {
+			this.infect(undefined, undefined);
+		}
+		
+		//Finish
+		helpers.after(replacement.__infection__);
+
+		stack.pop();
+
+		return toReturn;
+	};
+
+	//Set prototype correctly.
+	//ToDo: Set Constructor.
+	replacement.prototype = original.prototype;
+
+	//If this function was already distributed through code, we might accidentally infect/wrap it again.
+	//In that case, what we really want to do is update the function to be using the wrapper we already made.
+	Object.defineProperty(original, '__wrapper__', {value: replacement, enumerable:false });
+
+	//Attach properties.
+	for(var p in original) {
+		if(original.hasOwnProperty(p)) {
+			replacement[p] = original[p];
+		}
+	}
+
+	//Fix toString.
+	replacement.toString = function() {
+		return original.toString();
+	} 
+
+	//If this method has also been party to an infection.
+	replacement.toString.__infection__ = original.toString.__infection__;
+
+	//Return newly wrapped function.
+	return replacement;
 }
 
 //----------------------------------------------------------------------------------
@@ -178,33 +239,20 @@ function wrap(that, owner, name) {
 * 
 * @returns true or false based on whether or not the infection was successful.
 **/
-function buildInfection(that, owner, name) {
+function buildInfection(that, host, name) {
 	var __infection__;
 
 	if(!that) { return false; } //Base case.
 
 
-
-	if(typeof that === 'function') {
-
-		//A function that can't be infected will still be registered, it will just have a property on it that says, "we couldn't infect this"
-		//ToDo: make that actually be a thing.
-		that = wrap(that, owner, name);
-
-		//__infection__.creator
-	}
-
 	__infection__ = {
-		runs: 0, //How many times it's been called.
-		owner: owner, //Object that owns this function.
-		calledBy: [], //Other functions that call this one.
+		properties: {},
 		__ignore__: true
 
 	}
 
 	//We use defineProperty to keep our addition from being enumerated on in existing code.
     Object.defineProperty(that, '__infection__', { value : __infection__, enumerable: false});
-
 
 	return true;
 }
@@ -215,37 +263,50 @@ function buildInfection(that, owner, name) {
 * that should be a dead giveaway that the 'elsewhere' is a prototype of this object.
 * ToDo: This method should take an options object, not a set of parameters.
 * 
-* @param owner = optional parameter of a reference to the owning object.
+* @param host = optional parameter of a reference to the owning object.
 * @param name = optional name of this object in code.
 **/
-function infect(owner, name) {
+var globalIgnore = {
+	toString : true
+};
+function infect(host, name) {
 
-	if(!this.__infection__ && !this.__ignore__) {
+	if(!this.__ignore__) {
 
-		//console.log('Infecting ' + name + "(" + typeof this + "): " + " from " + owner);
-		buildInfection(this, owner, name);
+		if(!this.__infection__) {
+			buildInfection(this);
+		}
 
 	    for(var p in this) {
-	        if(this.hasOwnProperty(p)) {
-				this[p].infect(this, p);
-	        } else {
-	            this[p].infect(getOrigin(this.__proto__, p), p);
-	        }
+	    	if(!globalIgnore[p]) {
+		    	var properHost = this;
+		        if(!this.hasOwnProperty(p)) {
+		        	var properHost = getOrigin(this.__proto__, p);
+		        }
+
+				properHost[p] = wrapFunction(properHost[p]); //Will return original object if it's not a function.
+				properHost[p].infect(properHost, p);
+				properHost.infect.property(properHost, p);
+			}
 	    }
 	}
 };
 
-function cure(obj) {
+infect.property = infect
+
+function cure() {
 	//Cure an object of its infection.
 }
 
 function orphan() {
-	var n = orphanage.attach(this);
+	//var n = orphanage.attach(wrapFunction(this));
 
-	cure(this); //Should be an option.
-	this.infect(orphanage, n);
-
-	return orphanage[n]; //You've got him now, do what you wish.
+	//cure(this); //Should be an option.
+	//orphanage[n].infect(orphanage, n);
+	var n = wrapFunction(this);
+	n.infect();
+	return n;
+	//return orphanage[n]; //You've got him now, do what you wish.
 	//Register it as a private variable that will still be tracked.
 };
 
@@ -259,11 +320,20 @@ Object.defineProperty(Number.prototype, '__ignore__', {value: true, enumerable:f
 Object.defineProperty(Boolean.prototype, '__ignore__', {value: true, enumerable:false });
 
 Object.defineProperty(Object.prototype, 'infect', {value: infect, enumerable:false });
-Object.defineProperty(Object.prototype, 'orphan', {value: orphan, enumerable:false });
-Object.defineProperty(Object.prototype, 'ware', {value: helpers.get, enumerable:false});
+Object.defineProperty(Object.prototype, 'orphan', {value: orphan, enumerable:false }); //ToDo: this should be refactored to line up with Host concept.
+Object.infect.cure = cure;
+Object.infect.property = property;
+Object.infect.scope_wrap = localPropertyCode;
+
+
+
+//
+Object.defineProperty(Object.prototype, 'ware', {value: helpers.get, enumerable:false}); //ToDo: move someplace else.
+
 
 Object.prototype.orphan.__ignore__ = true;
 Object.prototype.infect.__ignore__ = true;
+Object.prototype.ware.__ignore__ = true;
 
 
 //-------------Register some base helpers----------------------
@@ -305,12 +375,12 @@ function detectChange(store) {
 	for(var v in store.watching) {
 		if(store.watching.hasOwnProperty(v)) {
 			watch = store.watching[v];
-			if(watch.owner[watch.name] !== watch.expected) {
-				//console.log('property changed: ' + watch.expected + " to " + watch.owner[watch.name]);
+			if(watch.host[watch.name] !== watch.expected) {
+				//console.log('property changed: ' + watch.expected + " to " + watch.host[watch.name]);
 				if(watch.onChange && typeof watch.onChange === 'function') {
-					watch.onChange(watch.expected, watch.owner[watch.name]);
+					watch.onChange(watch.expected, watch.host[watch.name]);
 				}
-				watch.expected = watch.owner[watch.name];
+				watch.expected = watch.host[watch.name];
 			}
 		}
 	}
@@ -329,11 +399,11 @@ helpers.register('tracker',
 		store.watching = {};
 		store.watchCount = 0;
 
-		var watch = function(property, owner, onChange) {
+		var watch = function(property, host, onChange) {
 			store.watching[store.watchCount] = {
 				'name': property,
-				'owner': owner,
-				'expected': owner[property],
+				'host': host,
+				'expected': host[property],
 				'onChange': onChange
 			};
 			return store.watchCount++;
